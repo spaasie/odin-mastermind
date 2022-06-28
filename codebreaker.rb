d691298d5a8e46811c3e77f4dbe2669a332a638f
spaasie <68701772+spaasie@users.noreply.github.com>
2022-06-28 12:36:47 +0200

    codebreaker logic working

--- codebreaker.rb
+++ codebreaker.rb
@@ -1,13 +1,14 @@
 module Codebreaker
   # game logic
   class Game
-    attr_reader :secret, :guesses, :current_player_id
+    attr_reader :secret, :guesses, :current_player_id, :clues
 
     def initialize(computer_class, human_class)
       @players = [computer_class.new('computer'), human_class.new('human')]
       @current_player_id = 0
       @secret = @players[0].create_code
       @guesses = []
+      @clues = []
     end
 
     def play
@@ -19,16 +20,48 @@ module Codebreaker
         end
 
         if computer?
+          if guesses.last == secret
+            puts "You win the secret is #{secret}"
+            return
+          end
           puts 'hello im a computer'
+          # eval guess
+          eval_guess if guesses.size.positive?
+
         elsif human?
           puts "You have #{rounds - guesses.size} rounds left"
-          puts guesses
+          guesses.each_with_index do |guess, i|
+            puts "#{guess} clues: #{clues[i]}"
+          end
           puts 'have a guess'
           print '> '
           guess(gets.chomp)
         end
         switch!
       end
+    end
+
+    def eval_guess
+      secret_arr = secret.split('')
+      guess_arr = guesses.last.split('')
+      a = 0
+      b = 0
+      a_indexes = []
+      secret_arr.each_with_index do |element, i|
+        if element == guess_arr[i]
+          a += 1
+          a_indexes.push(i)
+        end
+      end
+      a_indexes.reverse.each do |e|
+        secret_arr.delete_at(e)
+        guess_arr.delete_at(e)
+      end
+      # check remainder for the 'B'
+      secret_arr.each do |element|
+        b += 1 if guess_arr.include?(element)
+      end
+      clues.push("A#{a} B#{b}")
     end
 
     def guess(guess)
@@ -93,8 +126,6 @@ end
 
 include Codebreaker
 
-game = Game.new(Computer, Human)
+game = Game.new(Computer, Human).play
 
-puts game.secret
-
-game.play
+  # game.play
